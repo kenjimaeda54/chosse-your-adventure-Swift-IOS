a2676c8e7d52747e33112169e9cefe4195780fae
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-07-19 08:42:43 -0300

    feat: finished project

--- Chosse your adventure/Controllers/ViewController.swift
+++ Chosse your adventure/Controllers/ViewController.swift
@@ -13,17 +13,42 @@ class ViewController: UIViewController {
 	@IBOutlet weak var buttonChoice1: UIButton!
 	@IBOutlet weak var buttonChoice2: UIButton!
 	
+	let storyModel = StoryModel()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		let letter =  "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'."
-		let attributedString = NSMutableAttributedString(string: letter)
+	
+		setupViews()
+	
+	}
+	
+	func setupViews () {
+		let description = StoryModel.title
+		let attributedString = NSMutableAttributedString(string: description)
 		let paragraphStyle = NSMutableParagraphStyle()
 		paragraphStyle.lineSpacing = 4
 		paragraphStyle.lineHeightMultiple = 1.5
 		attributedString.addAttribute(.paragraphStyle,value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
 		labelDescription.attributedText = attributedString
+		buttonChoice1.setTitle(StoryModel.choice1, for: .normal)
+		buttonChoice2.setTitle(StoryModel.choice2, for: .normal)
+	
 	}
 	
+	@IBAction func pressChoice1(_ sender: UIButton) {
+		let story = StoryModel.story.filter({
+			 sender.currentTitle == $0.choice1
+		})
+		storyModel.handleDestination1(story[0].choice1Destionation)
+		setupViews()
+	}
 	
+	@IBAction func pressChoice2(_ sender: UIButton) {
+		let story = StoryModel.story.filter({
+			 sender.currentTitle == $0.choice2
+		})
+		storyModel.handleDestination1(story[0].choice2Destionation)
+		setupViews()
+	}
 }
 
